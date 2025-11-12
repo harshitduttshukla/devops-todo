072c0e41336fc5590844b368e5512b9ba347de61
Harshit Shukla <harshitshukla6388@gmail.com>
2025-11-12 22:21:51 +0530

    Fix puppet module path and update React deploy

--- puppet/modules/appdeploy/manifests/init.pp
+++ puppet/modules/appdeploy/manifests/init.pp
@@ -1,49 +1,35 @@
 class appdeploy {
-
-  # Install required tools
   package { ['curl', 'git']:
     ensure => installed,
   }
 
-  # Install Node.js (v18)
   exec { 'install_node':
     command => 'curl -fsSL https://deb.nodesource.com/setup_18.x | bash - && apt-get install -y nodejs',
     path    => ['/bin', '/usr/bin'],
     unless  => 'node -v',
   }
 
-  # Clone your React project from GitHub
   exec { 'clone_project':
-    command => 'git clone https://github.com/harshitduttshukla/FintaUi.git /opt/fintaui',
+    command => 'git clone https://github.com/harshitduttshukla/FintaUi.git /opt/todo',
     path    => ['/bin', '/usr/bin'],
-    creates => '/opt/fintaui',
+    creates => '/opt/todo',
   }
 
-  # Install dependencies using npm
   exec { 'install_dependencies':
-    command => 'cd /opt/fintaui && npm install',
+    command => 'cd /opt/todo && npm install && npm install -g serve',
     path    => ['/bin', '/usr/bin'],
     require => Exec['clone_project'],
   }
 
-  # Build the React project
-  exec { 'build_react_app':
-    command => 'cd /opt/fintaui && npm run build',
+  exec { 'build_react':
+    command => 'cd /opt/todo && npm run build',
     path    => ['/bin', '/usr/bin'],
     require => Exec['install_dependencies'],
   }
 
-  # Install serve globally (to serve the React build)
-  exec { 'install_serve':
-    command => 'npm install -g serve',
+  exec { 'start_react':
+    command => 'cd /opt/todo && serve -s build -l 8082 &',
     path    => ['/bin', '/usr/bin'],
-    unless  => 'which serve',
-  }
-
-  # Serve the built React app on port 3000
-  exec { 'start_react_app':
-    command => 'nohup serve -s /opt/fintaui/build -l 3000 &',
-    path    => ['/bin', '/usr/bin'],
-    require => Exec['build_react_app'],
+    require => Exec['build_react'],
   }
 }
