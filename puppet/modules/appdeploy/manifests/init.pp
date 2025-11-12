03381248005d4312e2a5db9ce17062163ca26747
Harshit Shukla <harshitshukla6388@gmail.com>
2025-11-12 16:51:03 +0530

    push full code

--- puppet/modules/appdeploy/manifests/init.pp
+++ puppet/modules/appdeploy/manifests/init.pp
@@ -1,37 +1,49 @@
 class appdeploy {
+
+  # Install required tools
   package { ['curl', 'git']:
     ensure => installed,
   }
 
-  # Install Node.js & npm
-  exec { 'install-node':
-    command => '/usr/bin/curl -fsSL https://deb.nodesource.com/setup_16.x | bash - && apt-get install -y nodejs',
+  # Install Node.js (v18)
+  exec { 'install_node':
+    command => 'curl -fsSL https://deb.nodesource.com/setup_18.x | bash - && apt-get install -y nodejs',
     path    => ['/bin', '/usr/bin'],
-    unless  => '/usr/bin/node -v',
-    require => Package['curl'],
+    unless  => 'node -v',
   }
 
-  # Clone the todo-list repository
-  exec { 'clone-todo':
-    command => '/usr/bin/git clone https://github.com/abdellatif-laghjaj/todo-list.git /opt/todo-list',
-    creates => '/opt/todo-list/package.json',
-    path    => ['/usr/bin', '/bin'],
-    require => [Package['git'], Exec['install-node']],
+  # Clone your React project from GitHub
+  exec { 'clone_project':
+    command => 'git clone https://github.com/harshitduttshukla/FintaUi.git /opt/fintaui',
+    path    => ['/bin', '/usr/bin'],
+    creates => '/opt/fintaui',
   }
 
-  # Install dependencies
-  exec { 'npm-install':
-    command => '/usr/bin/npm install',
-    cwd     => '/opt/todo-list',
-    path    => ['/usr/bin', '/bin'],
-    require => Exec['clone-todo'],
+  # Install dependencies using npm
+  exec { 'install_dependencies':
+    command => 'cd /opt/fintaui && npm install',
+    path    => ['/bin', '/usr/bin'],
+    require => Exec['clone_project'],
   }
 
-  # Start the Todo app
-  exec { 'start-app':
-    command => '/usr/bin/nohup npm start &',
-    cwd     => '/opt/todo-list',
-    path    => ['/usr/bin', '/bin'],
-    require => Exec['npm-install'],
+  # Build the React project
+  exec { 'build_react_app':
+    command => 'cd /opt/fintaui && npm run build',
+    path    => ['/bin', '/usr/bin'],
+    require => Exec['install_dependencies'],
+  }
+
+  # Install serve globally (to serve the React build)
+  exec { 'install_serve':
+    command => 'npm install -g serve',
+    path    => ['/bin', '/usr/bin'],
+    unless  => 'which serve',
+  }
+
+  # Serve the built React app on port 3000
+  exec { 'start_react_app':
+    command => 'nohup serve -s /opt/fintaui/build -l 3000 &',
+    path    => ['/bin', '/usr/bin'],
+    require => Exec['build_react_app'],
   }
 }
